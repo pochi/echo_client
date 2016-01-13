8893fdb59637aa2d2678b9a7c08585bfc4173933
pochi <pochi.black@gmail.com>
2016-01-13 10:39:38 +0900

    First commit

---
+++ cli.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "os"
+  "runtime"
+  "github.com/codegangsta/cli"
+)
+
+const (
+  Version = "0.0.1"
+)
+
+var (
+  app *cli.App
+)
+
+func main() {
+  runtime.GOMAXPROCS(runtime.NumCPU())
+  app = cli.NewApp()
+  app.Name = "pochi_echo"
+  app.Usage = "Echo message"
+  app.Action = func(c *cli.Context) {
+    println("Pochi")
+  }
+
+  app.Run(os.Args)
+}
